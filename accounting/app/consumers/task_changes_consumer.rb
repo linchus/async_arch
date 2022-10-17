c44bf7a173eb444495c9d2a1024825bb8a880d6f
Sergey Sein <ssein@servers.com>
2022-10-17 21:34:08 +0300

    Process task events

--- accounting/app/consumers/task_changes_consumer.rb
+++ accounting/app/consumers/task_changes_consumer.rb
@@ -8,6 +8,45 @@ class TaskChangesConsumer < ApplicationConsumer
       puts '-' * 80
       p message
       puts '-' * 80
+
+      case message['event_name']
+      when 'Task.Created' # CUD
+        # ignore
+      when 'Task.Updated' # CUD
+        task = Task.find_by(public_id: message['data']['public_id'])
+        next unless task
+
+        task.update!(title: message['data']['title'])
+      when 'Task.Added' # BE
+        Task.transaction do
+          task = Task.create!(
+            public_id: message['data']['public_id'],
+            title: message['data']['title']
+          )
+          user = User.find_or_create_by!(public_id: message['data']['assigned_to']['public_id'])
+          account = user.accounts.take || user.accounts.create!(
+            public_id: SecureRandom.uuid,
+            label: 'Default user account',
+            currency: Account::DEFAULT_CURRENCY
+          )
+          account.statements.create!(
+            credit: task.assign_price,
+            description: "#{task.title} assigned",
+            ref: task
+          )
+          task
+        end
+      when 'Task.Resolved' # BE
+        task = Task.find_by!(public_id: message['data']['public_id'])
+        user = User.find_by!(public_id: message['data']['assigned_to']['public_id'])
+        account = user.accounts.take
+        account.statements.create!(
+          debit: task.resolve_price,
+          description: "#{task.title} resolved",
+          ref: task
+        )
+        task
+      end
     end
   end
 end
--- accounting/app/models/task.rb
+++ accounting/app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  def initialize(*, **)
+    super
+
+    self.assign_price ||= rand(10) - 20
+    self.resolve_price ||= rand(20) + 20
+  end
 end
